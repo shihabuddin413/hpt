9937cb15fe14184df9c103a6f43d0f327cbe5eec
shihabe uddin <65333445+shihabuddin413@users.noreply.github.com>
2025-05-11 02:40:59 +0100

    port binding

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ def home():
     return "Hello Bot is running!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
